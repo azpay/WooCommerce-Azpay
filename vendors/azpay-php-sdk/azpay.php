1a5cd4704c12761c08686057a5633b437b88be54
Gabriel Guerreiro <gaguerreiro@gmail.com>
2015-05-24 19:37:19 -0300

    AZPay SDK Update

--- vendors/azpay-php-sdk/azpay.php
+++ vendors/azpay-php-sdk/azpay.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * AZPay SDK
- * 
- * Version: 1.0.0
- * Author: Gabriel Guerreiro
- * Copyright AZClick
- * 
- */
+ *
+ * Software Development Kit to integrate a checkout with AZPay Gateway
+ *
+ * @author Gabriel Guerreiro <gabrielguerreiro.com>
+ * @version 1.2.1
+ **/
 
 include 'config.php';
 include 'utils.php';
@@ -14,143 +14,262 @@ include 'xml_requests.php';
 
 class AZPay {
 
+
 	/**
 	 * VERSION
 	 */
-	const VERSION = '1.0.0';
+	const VERSION = '1.2.1';
 
 	/**
 	 * Client Key and Client ID
+	 *
 	 * @var array
 	 */
 	public $merchant = array(
-		'id' => '',
-		'key' => ''
+		'id' 	=> '',
+		'key' 	=> ''
 	);
+
 
 	/**
 	 * Order Details
+	 *
 	 * @var array
 	 */
 	public $config_order = array(
-		'reference' => '',
-		'totalAmount' => '0000'
+		'reference' 	=> '',
+		'totalAmount' 	=> '0000'
 	);
+
+
+	/**
+	 * Rebill configuration
+	 *
+	 * @var array
+	 */
+	public $config_rebill = array(
+		'period' 	=> '1',
+		'frequency'	=> '12',
+		'dateStart'	=> '2015-01-01',
+		'dateEnd' 	=> '2014-01-01'
+	);
+
 
 	/**
 	 * Billing Details
+	 *
 	 * @var array
 	 */
 	public $config_billing = array(
-		'customerIdentity' => '',
-		'name' => '',
-		'address' => '',
-		'address2' => '',
-		'city' => '',
-		'state' => '',
-		'postalCode' => '',
-		'country' => 'BR',
-		'phone' => '',
-		'email' => ''
+		'customerIdentity' 	=> '',
+		'name' 				=> '',
+		'address' 			=> '',
+		'address2' 			=> '',
+		'city' 				=> '',
+		'state' 			=> '',
+		'postalCode' 		=> '',
+		'country' 			=> 'BR',
+		'phone' 			=> '',
+		'email' 			=> ''
 	);
+
 
 	/**
 	 * Creditcard data
+	 *
 	 * @var array
 	 */
 	public $config_card_payments = array(
-		'acquirer' => '',
-		'method' => '',
-		'amount' => '0000',
-		'currency' => '986',
-		'country' => 'BRA',
-		'numberOfPayments' => '1',
-		'groupNumber' => '0',
-		'flag' => '',
-		'cardHolder' => '',
-		'cardNumber' => '',
-		'cardSecurityCode' => '',
-		'cardExpirationDate' => '',
-		'saveCreditCard' => 'true',
-		'generateToken' => 'false',
-		'departureTax' => '0',
-		'softDescriptor' => '',
-		'tokenCard' => ''
+		'acquirer' 				=> '',
+		'method' 				=> '',
+		'amount' 				=> '0000',
+		'currency' 				=> '986',
+		'country' 				=> 'BRA',
+		'numberOfPayments' 		=> '1',
+		'groupNumber' 			=> '0',
+		'flag' 					=> '',
+		'cardHolder' 			=> '',
+		'cardNumber' 			=> '',
+		'cardSecurityCode' 		=> '',
+		'cardExpirationDate' 	=> '',
+		'saveCreditCard' 		=> 'true',
+		'generateToken' 		=> 'false',
+		'departureTax' 			=> '0',
+		'softDescriptor' 		=> '',
+		'tokenCard' 			=> ''
 	);
+
 
 	/**
 	 * Boleto configuration
+	 *
 	 * @var array
 	 */
 	public $config_boleto = array(
-		'acquirer' => '10',
-		'expire' => '',
-		'nrDocument' => '',
-		'amount' => '000',
-		'currency' => '986',
-		'country' => 'BRA',
-		'instructions' => ''
+		'acquirer'	 	=> '10',
+		'expire' 		=> '',
+		'nrDocument' 	=> '',
+		'amount'		=> '0000',
+		'currency' 		=> '986',
+		'country' 		=> 'BRA',
+		'instructions' 	=> ''
+	);
+
+
+	/**
+	 * PagSeguro configuration
+	 *
+	 * @var array
+	 */
+	public $config_pagseguro = array(
+		'amount' 	=> '0000',
+		'currency' 	=> '986',
+		'country' 	=> 'BRA'
+	);
+
+
+	/**
+	 * PagSeguro Creditcard configuration
+	 *
+	 * @var array
+	 */
+	public $config_pagseguro_checkout = array(
+		'method' 				=> '',
+		'amount' 				=> '0000',
+		'currency' 				=> '986',
+		'country' 				=> 'BRA',
+		'numberOfPayments' 		=> '1',
+		'flag' 					=> '',
+		'cardHolder' 			=> '',
+		'cardNumber' 			=> '',
+		'cardSecurityCode' 		=> '',
+		'cardExpirationDate' 	=> ''
 	);
 
 	/**
+	 * Billing Details to PagSeguro
+	 *
+	 * @var array
+	 */
+	public $config_pagseguro_checkout_billing = array(
+		'customerIdentity' 	=> '',
+		'name' 				=> '',
+		'address' 			=> '',
+		'address2' 			=> '',
+		'city' 				=> '',
+		'state' 			=> '',
+		'postalCode'		=> '',
+		'country' 			=> 'BR',
+		'phone' 			=> '',
+		'email' 			=> '',
+		'birthDate' 		=> '',
+		'cpf' 				=> ''
+	);
+
+
+	/**
+	 * PayPal configuration
+	 *
+	 * @var array
+	 */
+	public $config_paypal = array(
+		'amount' 	=> '0000',
+		'currency' 	=> '986',
+		'country' 	=> 'BRA'
+	);
+
+
+	/**
+	 * Online Debit configuration
+	 *
+	 * @var array
+	 */
+	public $config_online_debit = array(
+		'acquirer' 	=> ''
+	);
+
+
+	/**
 	 * Options extra
+	 *
 	 * @var array
 	 */
 	public $config_options = array(
-		'urlReturn' => '',
-		'fraud' => 'false',
-		'customField' => ''
+		'urlReturn' 	=> '',
+		'fraud'		 	=> 'false',
+		'customField' 	=> ''
 	);
 
 
 	/**
 	 * Reponse
-	 * @var [type]
+	 *
+	 * @var String
 	 */
-	public $curl_response;
+	private $curl_response = null;
 
 	/**
 	 * Meta data
-	 * @var [type]
+	 *
+	 * @var String
 	 */
-	public $curl_response_meta;
+	private $curl_response_meta = null;
 
 	/**
 	 * Errors
-	 * @var [type]
+	 *
+	 * @var String
 	 */
-	public $curl_error;	
+	private $curl_error = null;
 
 	/**
 	 * Errors Code
-	 * @var [type]
+	 *
+	 * @var int
 	 */
-	public $curl_error_code;	
+	private $curl_error_code = 0;
+
+	/**
+	 * Set timeout to cURL
+	 * default = 5s
+	 *
+	 * @var int
+	 */
+	public $curl_timeout = 5;
 
 	/**
 	 * Error flag
+	 *
 	 * @var boolean
 	 */
 	public $error = false;
 
+
 	/**
 	 * Flag to execute Exceptions
+	 *
 	 * @var boolean
 	 */
 	public $throw_exceptions = true;
 
+	/**
+	 * XML generated
+	 * @var XMLObject
+	 */
+	private $xml = null;
+
 
 
 	/**
-	 * Construct class
-	 * 
-	 * @param [type] $merchant_id  [Client ID]
-	 * @param [type] $merchant_key [Client Key]
+	 * Construct Class
+	 *
+	 * @param string $merchant_id  [Client ID]
+	 * @param string $merchant_key [Client Key]
 	 */
 	function __construct($merchant_id, $merchant_key) {
 
 		if (!function_exists('curl_init'))
-            throw new Exception('CURL module not available! Pest requires CURL. See http://php.net/manual/en/book.curl.php');        
+            throw new Exception('cURL module is not available! This SDK requires cURL. See http://php.net/manual/en/book.curl.php');
 
 		$this->merchant['id'] = $merchant_id;
 		$this->merchant['key'] = $merchant_key;
@@ -158,18 +277,23 @@ class AZPay {
 
 
 	/**
-	 * Execute request
-	 * 
-	 * @return [type] [execute cUrl request]
+	 * Execute Request
+	 * Use after all to make request in fact
+	 *
+	 * eg: $azpay->sale()->execute()
+	 *
+	 * @return this
 	 */
-	public function execute($xml) {		
+	public function execute() {
 
 		$ch = curl_init();
-		
+
 		curl_setopt($ch, CURLOPT_URL, Config::$RECIVER_URL);
 		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $this->xml);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+		curl_setopt($ch, CURLOPT_TIMEOUT, $this->curl_timeout);
 
 		$this->curl_response = curl_exec($ch);
 		$this->curl_response_meta = curl_getinfo($ch);
@@ -180,8 +304,8 @@ class AZPay {
 			$this->curl_error_code = curl_errno($ch);
 		}
 
-		if ($this->curl_response === false && $this->throw_exceptions)			
-			throw new AZPay_Curl_Exec_Exception(curl_error($ch));		
+		if ($this->curl_response === false && $this->throw_exceptions)
+			throw new AZPay_Curl_Exec_Exception(curl_error($ch));
 
 		if ($this->curl_response_meta === false && $this->throw_exceptions)
 			throw new AZPay_Curl_Meta_Exception(curl_error($ch));
@@ -189,20 +313,24 @@ class AZPay {
 		curl_close($ch);
 
 		$this->checkErrors();
+
+		return $this;
 	}
 
 
 	/**
-	 * Return the cUrl response 
-	 * 
-	 * @return [type] [description]
+	 * Return XML response
+	 * from AZPay
+	 *
+	 * @return XMLObject if [Reponse]
+	 * @return string if [Error]
 	 */
 	public function response() {
 
 		// If no errors, return XML parsed
-		if ($this->error == false) {
-			
-			$xml = simplexml_load_string($this->curl_response);
+		if ($this->curl_error === null) {
+
+			$xml = simplexml_load_string($this->curl_response, 'SimpleXMLElement', LIBXML_NOCDATA);
 
 			return $xml;
 
@@ -211,119 +339,370 @@ class AZPay {
 			return $this->curl_error;
 
 		}
-
 	}
 
 
 	/**
-	 * Request authorization to complete the transaction
-	 * 	 
-	 * @return [type]          [description]
+	 * Get AZPay errors response
+	 * use inside Catch
+	 *
+	 * @return array [Data from errors]
+	 */
+	public function responseError() {
+
+		$xml = simplexml_load_string($this->curl_response, 'SimpleXMLElement', LIBXML_NOCDATA);
+		$json = json_decode(json_encode($xml));
+
+		$response = array(
+			'status_code' 		=> $json->status->code,
+			'status_message' 	=> $json->status->message,
+			'error_code' 		=> $json->result->error->code,
+			'error_action' 		=> $json->result->error->action,
+			'error_details'		=> str_replace(array('<![CDATA[', ']]>'), '', $json->result->error->details),
+			'error_moreInfo'	=> str_replace(array('<![CDATA[', ']]>'), '', $json->result->error->moreInfo),
+			'error_message'		=> Config::$ERROR_MESSAGE[$json->result->error->code], // Message error to view
+		);
+
+		// 101 - XML Error
+		if ($json->result->error->code != '101' && $json->result->error->code != 101) {
+			$response['message_acquirer'] = $json->result->error->message->acquirer;
+			$response['error_acquirer'] = $json->result->error->message->errorAcquirer;
+			$response['timestamp'] = $json->result->error->message->timestamp;
+		}
+
+		return $response;
+	}
+
+
+	/**
+	 * Get XML generated
+	 * Use after any Request and before Execute
+	 *
+	 * eg: $azpay->sale()->getXml()
+	 *
+	 * @return XMLObject
+	 */
+	public function getXml() {
+		return $this->xml;
+	}
+
+
+	/**
+	 * Get cURL response
+	 * 
+	 * @return string
+	 */
+	public function getCurlResponse() {
+		return $this->curl_response;
+	}
+
+
+	/**
+	 * Get cURL response meta
+	 * 
+	 * @return string
+	 */
+	public function getCurlResponseMeta() {
+		return $this->curl_response_meta;
+	}
+
+
+	/**
+	 * Get cURL Error
+	 * 
+	 * @return string
+	 */
+	public function getCurlError() {
+		return $this->curl_error;
+	}
+
+
+	/**
+	 * Get cURL Error Code
+	 * 
+	 * @return string
+	 */
+	public function getCurlErrorCode() {
+		return $this->curl_error_code;
+	}
+
+
+	/**
+	 * Flag to ignore exceptions
+	 * user before any Request
+	 *
+	 * eg: $azpay->ignoreExceptions()->sale()
+	 * 
+	 * @return this
+	 */
+	public function ignoreExceptions() {
+		$this->throw_exceptions = false;
+		return $this;
+	}
+
+
+	/**
+	 * Request authorization
+	 * to complete the transaction
+	 *
+	 * Require:
+	 * 	- $merchant
+	 * 	- $config_order
+	 * 	- $config_card_payments
+	 * 	- $config_billing
+	 * 	- $config_options
+	 *
+	 * @return this
 	 */
 	public function authorize() {
 
 		$requests = new XML_Requests();
-		
+
 		$requests->authorizeXml($this->merchant, $this->config_order, $this->config_card_payments, $this->config_billing, $this->config_options);
-		$xml = $requests->output();
+		$this->xml = $requests->output();
 
-		$this->execute($xml);
-
+		return $this;
 	}
+
 
 	/**
 	 * Request the capture of the transaction
-	 * after the authorization request, to payment validation
-	 * 
-	 * @return [type] [description]
+	 * after the authorization request
+	 * to payment validation
+	 *
+	 * Require:
+	 * 	- $merchant
+	 *
+	 * @param  string $transactionId [TID of AZPay transaction]
+	 * @return this
 	 */
 	public function capture($transactionId) {
 
 		$requests = new XML_Requests();
-		
+
 		$requests->captureXml($this->merchant['id'], $this->merchant['key'], $transactionId);
-		$xml = $requests->output();
+		$this->xml = $requests->output();
 
-		$this->execute($xml);
-
+		return $this;
 	}
 
 
 	/**
 	 * Request Sale
 	 * without a pre authorization
-	 * 
-	 * @return [String] [description]
+	 *
+	 * Require:
+	 *  - $merchant
+	 *  - $config_order
+	 *  - $config_card_payments
+	 *  - $config_billing
+	 *  - $config_options
+	 *
+	 * @return this
 	 */
 	public function sale() {
 
 		$requests = new XML_Requests();
-		
+
 		$requests->saleXml($this->merchant, $this->config_order, $this->config_card_payments, $this->config_billing, $this->config_options);
-		$xml = $requests->output();
+		$this->xml = $requests->output();
 
-		$this->execute($xml);
-
+		return $this;
 	}
 
 
 	/**
-	 * Request Report
-	 * to check the transaction status
-	 * 
-	 * @param  [type] $tid [TID]
-	 * @return [type]      [description]
+	 * Request Rebill
+	 *
+	 * Require:
+	 *  - $merchant
+	 *  - $config_order
+	 *  - $config_card_payments
+	 *  - $config_billing
+	 *  - $config_options
+	 *  - $config_rebill
+	 *
+	 * @return this
 	 */
-	public function report($tid) {
+	public function rebill() {
 
 		$requests = new XML_Requests();
-		
-		$requests->reportXml($this->merchant, $tid);
-		$xml = $requests->output();
 
-		$this->execute($xml);
+		$requests->creditcardRebillXml($this->merchant, $this->config_order, $this->config_card_payments, $this->config_billing, $this->config_options, $this->config_rebill);
+		$this->xml = $requests->output();
 
+		return $this;
 	}
 
 
 	/**
-	 * Request Cancel
-	 * 
-	 * @param  [type] $tid [description]
-	 * @return [type]      [description]
+	 * Request a transaction status report
+	 *
+	 * Require:
+	 * 	- $merchant
+	 *
+	 * @param  string $transactionId [TID of AZPay transaction]
+	 * @return this
 	 */
-	public function cancel($tid) {
+	public function report($transactionID) {
 
 		$requests = new XML_Requests();
-		
-		$requests->cancelXml($this->merchant, $tid);
-		$xml = $requests->output();
 
-		$this->execute($xml);
+		$requests->reportXml($this->merchant, $transactionID);
+		$this->xml = $requests->output();
 
+		return $this;
+	}
+
+
+	/**
+	 * Request a Transaction cancel
+	 *
+	 * Require:
+	 * 	- $merchant
+	 *
+	 * @param  string $transactionID [TID of AZPay transaction]
+	 * @return this
+	 */
+	public function cancel($transactionID) {
+
+		$requests = new XML_Requests();
+
+		$requests->cancelXml($this->merchant, $transactionID);
+		$this->xml = $requests->output();
+
+		return $this;
 	}
 
 
 	/**
 	 * Request Boleto
-	 * 
-	 * @return [type] [description]
+	 *
+	 * Require:
+	 * 	- $merchant
+	 *  - $config_order
+	 *  - $config_boleto
+	 *  - $config_billing
+	 *  - $config_options
+	 *
+	 * To Rebill require:
+	 * 	- $config_rebill
+	 *
+	 * @param  [boolean] $rebill [Flag to enable rebill]
+	 * @return this
 	 */
-	public function boleto() {
+	public function boleto($rebill = false) {
 
 		$requests = new XML_Requests();
-		
-		$requests->boletoXml($this->merchant, $this->config_order, $this->config_boleto, $this->config_billing, $this->config_options);
-		$xml = $requests->output();
 
-		$this->execute($xml);
+		if ($rebill) {
+			$requests->boletoRebillXml($this->merchant, $this->config_order, $this->config_boleto, $this->config_billing, $this->config_options, $this->config_rebill);
+		} else {
+			$requests->boletoXml($this->merchant, $this->config_order, $this->config_boleto, $this->config_billing, $this->config_options);
+		}
 
+		$this->xml = $requests->output();
+
+		return $this;
 	}
+
+
+	/**
+	 * PagSeguro
+	 *
+	 * Require:
+	 *  - $merchant
+	 *  - $config_order
+	 *  - $config_pagseguro
+	 *  - $config_billing
+	 *  - $config_options
+	 *
+	 * @return this
+	 */
+	public function pagseguro() {
+
+		$requests = new XML_Requests();
+
+		$requests->pagseguroXml($this->merchant, $this->config_order, $this->config_pagseguro, $this->config_billing, $this->config_options);
+		$this->xml = $requests->output();
+
+		return $this;
+	}
+
+
+	/**
+	 * PagSeguro Checkout
+	 *
+	 * Require:
+	 *  - $merchant
+	 *  - $config_order
+	 *  - $config_pagseguro_checkout
+	 *  - $config_pagseguro_billing
+	 *  - $config_options
+	 *
+	 * @return this
+	 */
+	public function pagseguro_checkout() {
+
+		$requests = new XML_Requests();
+
+		$requests->pagseguroCheckoutXml($this->merchant, $this->config_order, $this->config_pagseguro_checkout, $this->config_pagseguro_checkout_billing, $this->config_options);
+		$this->xml = $requests->output();
+
+		return $this;
+	}
+
+
+	/**
+	 * PayPal Checkout
+	 *
+	 * Require:
+	 *  - $merchant
+	 *  - $config_order
+	 *  - $config_paypal
+	 *  - $config_billing
+	 *  - $config_options
+	 *
+	 * @return this
+	 */
+	public function paypal() {
+
+		$requests = new XML_Requests();
+
+		$requests->paypalXml($this->merchant, $this->config_order, $this->config_paypal, $this->config_billing, $this->config_options);
+		$this->xml = $requests->output();
+
+		return $this;
+	}
+
+
+	/**
+	 * Online Debit
+	 *
+	 * Require:
+	 *  - $merchant
+	 *  - $config_order
+	 *  - $config_pagseguro_checkout
+	 *  - $config_pagseguro_billing
+	 *  - $config_options
+	 *
+	 * @return this
+	 */
+	public function online_debit() {
+
+		$requests = new XML_Requests();
+
+		$requests->onlineDebitXml($this->merchant, $this->config_order, $this->config_online_debit, $this->config_billing, $this->config_options);
+		$this->xml = $requests->output();
+
+		return $this;
+	}
+
 
 	/**
 	 * Check error exceptions
-	 * 
-	 * @return [void] [description]
+	 *
+	 * @return void
 	 */
 	private function checkErrors() {
 
@@ -353,7 +732,7 @@ class AZPay {
                 throw new AZPay_MethodNotAllowed($response);
                 break;
             case 409:
-                throw new AZPay_Conflict($response);
+                throw new AZPay_Error($response);
                 break;
             case 410:
                 throw new AZPay_Gone($response);
@@ -395,7 +774,7 @@ class AZPay_Unauthorized extends AZPay_Client_Exception {}
 class AZPay_Forbidden extends AZPay_Client_Exception {}
 class AZPay_NotFound extends AZPay_Client_Exception {}
 class AZPay_MethodNotAllowed extends AZPay_Client_Exception {}
-class AZPay_Conflict extends AZPay_Client_Exception {}
+class AZPay_Error extends AZPay_Client_Exception {}
 class AZPay_Gone extends AZPay_Client_Exception {}
 class AZPay_InvalidRecord extends AZPay_Client_Exception {}
 class AZPay_ServerError extends AZPay_Client_Exception {}
@@ -404,8 +783,9 @@ class AZPay_UnknownResponse extends AZPay_Client_Exception {}
 /**
  * cUrl Exceptions
  */
-class AZPay_Curl_Init_Exception extends AZPay_Exception {}
-class AZPay_Curl_Meta_Exception extends AZPay_Exception {}
-class AZPay_Curl_Exec_Exception extends AZPay_Exception {}
+class AZPay_Curl_Exception extends AZPay_Exception {}
+class AZPay_Curl_Init_Exception extends AZPay_Curl_Exception {}
+class AZPay_Curl_Meta_Exception extends AZPay_Curl_Exception {}
+class AZPay_Curl_Exec_Exception extends AZPay_Curl_Exception {}
 
 ?>
--- vendors/azpay-php-sdk/config.php
+++ vendors/azpay-php-sdk/config.php
@@ -1,40 +1,59 @@
-<?php 
+<?php
 /**
- * Config
+ * Config Class
  *
- * Classe de constantes e configuracões
- * 
- */
+ * Static attributes with configurations, operations and messages
+ *
+ * @author Gabriel Guerreiro <gabrielguerreiro.com>
+ **/
 
 class Config {
 
-	# AZPay reciver url
+	/**
+   * AZPay URL
+   *
+   * @var string
+   */
 	public static $RECIVER_URL = 'https://api.azpay.com.br/v1/receiver/';
 
-	# Version
+
+	/**
+   * AZPay Version
+   *
+   * @var string
+   */
   public static $AZPAY_VERSION = '1.0.0';
 
-  # Flags
+
+  /**
+   * Cards Flags
+   * @var array
+   */
   public static $FLAGS = array(
-    'visa' => 'Visa',
-    'mastercard' => 'MasterCard',
-    'amex' => 'Amex',
-    'elo' => 'Elo',
-    'dinners' => 'Dinners',
-    'discover' => 'Discover',
-    'jcb' => 'JCB',
-    'aura' => 'Aura'
+    'visa'        => 'Visa',
+    'mastercard'  => 'MasterCard',
+    'amex'        => 'Amex',
+    'elo'         => 'Elo',
+    'dinners'     => 'Dinners',
+    'discover'    => 'Discover',
+    'jcb'         => 'JCB',
+    'aura'        => 'Aura'
   );
 
-  # AZPay Cards Operators
+
+  /**
+   * AZPay Cards Operators
+   *
+   * @var array
+   */
   public static $CARD_OPERATORS = array(
+
     'cielo' => array(
       'name' => 'Cielo',
       'modes' => array(
       	'store' => array('code' => '1', 'name' => 'Buy Store'),
       	'cielo' => array('code' => '2', 'name' => 'Buy Cielo')
-      )
-      ,
+      ),
       'flags' => array(
         'visa',
         'mastercard',
@@ -46,6 +65,7 @@ class Config {
         'aura'
       )
     ),
+
     'redecard' => array(
       'name' => 'RedeCard',
       'modes' => array(
@@ -57,8 +77,18 @@ class Config {
         'mastercard'
       )
     ),
+
     'elavon' => array(
       'code' => '6',
+      'name' => 'Elavon',
+      'flags' => array(
+        'visa',
+        'mastercard'
+      )
+    ),
+
+    'stone' => array(
+      'code' => '20',
       'name' => 'Elavon',
       'flags' => array(
         'visa',
@@ -67,7 +97,12 @@ class Config {
     )
   );
 
-  # AZPay boleto operators
+
+  /**
+   * AZPay Boleto operators
+   *
+   * @var array
+   */
   public static $BOLETO_OPERATORS = array(
     'bradesco' => array(
       'code' => '10',
@@ -107,12 +142,22 @@ class Config {
     )
   );
 
-  # Curency
+
+  /**
+   * Currency
+   *
+   * @var array
+   */
   public static $CURRENCIES = array(
     'BRL' => 986
   );
 
-  # Operations Methods
+
+  /**
+   * Operation Methods
+   *
+   * @var array
+   */
   public static $OPERATION_METHODS = array(
     '1' => array(
       'name' => 'Crédito a vista'
@@ -128,28 +173,13 @@ class Config {
     )
   );
 
-  # Operation Types
-  public static $ORDER_STATUS = array(
-    '0' => 'Criada / Em andamento',
-    '1' => 'Autenticada',
-    '2' => 'Não Autenticada',
-    '3' => 'Autorizada pela operadora',
-    '4' => 'Não autorizada pela operadora',
-    '5' => 'Em Cancelamento',
-    '6' => 'Cancelado',
-    '7' => 'Em Captura',
-    '8' => 'Capturada / Finalizada',
-    '9' => 'Não capturada',
-    '10' => 'Pagamento Recorrente - Agendado',
-    '12' => 'Boleto Gerado'
-  );
-
 
   /**
    * Code responses by flag
+   *
    * @var array
    */
-  public static $RESPONSE = array(
+  public static $STATUS = array(
     'CREATED'         => 0,
     'AUTHENTICATED'   => 1,
     'UNAUTHENTICATED' => 2,
@@ -164,8 +194,67 @@ class Config {
     'GENERATED'       => 12
   );
 
+
+  /**
+   * Response messages by status code
+   * 
+   * @var array
+   */
+  public static $STATUS_MESSAGES = array(
+    0 => array(
+      'title' => 'Criada / Em andamento',
+      'message' => 'A transação ainda não foi processada pelo Adquirente escolhido para transacionar.',
+    ),
+    1 => array(
+      'title' => 'Autenticada',
+      'message' => 'A transação foi validada mais ainda não foi processada para saber se foi aprovada.',
+    ),
+    2 => array(
+      'title' => 'Não Autenticada',
+      'message' => 'Não houve validação do cartão de crédito.'
+    ),
+    3 => array(
+      'title' => 'Autorizada pela operadora',
+      'message' => 'A transação foi aprovada mais ainda não foi capturada, os créditos ainda não foram creditados para o lojista.'
+    ),
+    4 => array(
+      'title' => 'Não Autorizada pela operadora',
+      'message' => 'A transação não foi aprovada, verifique a tabela de possíveis erros.'
+    ),
+    5 => array(
+      'title' => 'Em Cancelamento',
+      'message' => 'A transação ainda não foi cancelada, aguardando adquirente retornar a confirmação.'
+    ),
+    6 => array(
+      'title' => 'Cancelado',
+      'message' => 'A transação foi cancelada e não será mais creditada o valor para o lojista.'
+    ),
+    7 => array(
+      'title' => 'Em Captura',
+      'message' => 'A transação ainda não foi capturada, aguardando adquirente retornar a confirmação.'
+    ),
+    8 => array(
+      'title' => 'Capturada / Finalizada',
+      'message' => 'A transação foi capturada e os créditos foram confirmados para o lojista.'
+    ),
+    9 => array(
+      'title' => 'Não Capturada',
+      'message' => 'A transação não foi capturada, tentar novamente a captura.'
+    ),
+    10 => array(
+      'title' => 'Pagamento Recorrente - Agendado',
+      'message' => 'A transação de pagamento foi agendada para as datas definidas pelo envio do XML.'
+    ),
+    12 => array(
+      'title' => 'Boleto Gerado',
+      'message' => 'O boleto foi gerado com sucesso.'
+    )
+  );
+
+
   /**
    * Code operations by flag
+   *
    * @var array
    */
   public static $OPERATION = array(
@@ -179,7 +268,67 @@ class Config {
     'AUTH'      => 9,
     'PAGSEGURO' => 10,
     'PAYPAL'    => 11,
-    'TRANSFER'  => 12,
+    'TRANSFER'  => 12
+  );
+
+
+  /**
+   * Period options to Rebill
+   * 
+   * @var array
+   */
+  public static $REBILL_PERIOD = array(
+  	'DAY' 	=> 1,
+  	'WEEK' 	=> 2,
+  	'MONTH' => 3,
+  	'YEAR' 	=> 4,
+  );
+
+
+  /**
+   * Error Messages by code
+   *
+   * @var array
+   */
+  public static $ERROR_MESSAGE = array(
+    '001' => 'O XML enviado está com erros',
+    '002' => 'O XML enviado está com restrições na CIELO',
+    '003' => 'O XML enviado está com restrições na REDE',
+    '010' => 'Autenticação no AZPAY inválidas',
+    '011' => 'Autenticação na CIELO inválidas',
+    '012' => 'Autenticação na REDE inválidas',
+    '020' => 'Inconsistência no envio do cartão, verifique a modalidade habilitada (BPL)',
+    '021' => 'Modalidade de pagamento não habilitada na CIELO',
+    '022' => 'Modalidade de pagamento não habilitada na REDE',
+    '023' => 'Número de parcelas inválidos ou limite máximo permitido junto à CIELO foi ultrapassado',
+    '024' => 'Número de parcelas inválidos ou limite máximo permitido junto à REDE foi ultrapassado',
+    '025' => 'Código de segurança ausente ou inválido',
+    '026' => 'Erro ao processar o cartão',
+    '027' => 'Indicador do código de segurança é inválido',
+    '028' => 'Inconsistência no envio do cartão, dados inválidos',
+    '030' => 'Código de Transação inválido',
+    '031' => 'Identificador, TID, inválido na CIELO',
+    '032' => 'Código de Transação inválido na REDE',
+    '033' => 'Falha ao cancelar a transação',
+    '034' => 'Transação já está cancelada',
+    '035' => 'Transação não encontrada para realizar esta operação',
+    '036' => 'Transação já está capturada',
+    '037' => 'Adquirente informado não está configurado corretamente',
+    '100' => 'Falha ao tentar conectar no servidor da operadora - Timeout',
+    '101' => 'Transação inválida',
+    '900' => 'Moeda inválida',
+    '901' => 'Idioma inválido',
+    '902' => 'DataHora inválida',
+    '903' => 'Número do pedido inválido',
+    '904' => 'Valor do pedido inválido',
+    '905' => 'Caracteres inválidos no nome do portador',
+    '906' => 'Data de validade inválida',
+    '907' => 'Bandeira inválida',
+    '908' => 'Indicador de gerar token inválido',
+    '909' => 'Taxa de embarque inválida',
+    '910' => 'Data final é inválida',
+    '911' => 'Data inicial é inválida, data precisa ser próximo dia',
+    '912' => 'Bandeira utilizada não permite operação de autorização na Cielo'
   );
 
 }
